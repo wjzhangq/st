4d39f3963af4036b54101ca787d34b6f6cc9252e
Justin <wenjin.zhang@gmail.com>
2010-07-15 10:30:21 +0800

    add_param for uri

--- framework/class/core/uri.php
+++ framework/class/core/uri.php
@@ -1,102 +1,102 @@
 <?php
 class Uri extends object {
-	private $var = array();
-	
-	static function getInstance(){
-		return parent::getInstance(__CLASS__);
-	}
-	
-	public function __static(){
-		
-	}
-	
-	function parse($uri=null){
-		if (!$uri){
-			$uri = self::get_uri_string();		
-		}
-		$uri_arr = explode('/', $uri);
-		
-		$file_path = '';
-		
-		$len = count($uri_arr);
-		if (!$uri){
-			$class_name = 'index';
-			$file_path = APP . '/page/index.php';
-		}else{
-			$file_path = APP . '/page';
-			for($i=0; $i <$len; $i++){
-				if (is_dir($file_path . '/' . $uri_arr[$i])){
-					$file_path .= '/' . $uri_arr[$i];
-				}else{
-					$class_name = $uri_arr[$i];
-					$file_path .= '/' . $class_name . '.php';
-					if ($i < $len){
-						$rUri = implode('/', array_slice($uri_arr, $i+1));
-					}
-					break;
-				}
-			}
-			//遍历到最后的
-			if (empty($class_name)){
-				$class_name = 'index';
-				$file_path .= '/' . $class_name . '.php';
-			}
-		}
-		
-		return array('path'=>$file_path, 'class'=>$class_name, 'rUri'=>isset($rUri) ? $rUri : '');
-	}
-	
-	function get_request_uri(){
-		$start_url = ( isset($_SERVER['REQUEST_URI'])
-					? $_SERVER['REQUEST_URI']
-					: $_SERVER['SCRIPT_NAME'] .
-						( isset($_SERVER['PATH_INFO'])
-						? $_SERVER['PATH_INFO']
-						: '') .
-							( (isset($_SERVER['QUERY_STRING']) && ($_SERVER['QUERY_STRING'] != ''))
-							? '?' . $_SERVER['QUERY_STRING']
-							: ''));
-		return $start_url;
-	}
+    private $var = array();
+    
+    static function getInstance(){
+        return parent::getInstance(__CLASS__);
+    }
+    
+    public function __static(){
+        
+    }
+    
+    function parse($uri=null){
+        if (!$uri){
+            $uri = self::get_uri_string();      
+        }
+        $uri_arr = explode('/', $uri);
+        
+        $file_path = '';
+        
+        $len = count($uri_arr);
+        if (!$uri){
+            $class_name = 'index';
+            $file_path = APP . '/page/index.php';
+        }else{
+            $file_path = APP . '/page';
+            for($i=0; $i <$len; $i++){
+                if (is_dir($file_path . '/' . $uri_arr[$i])){
+                    $file_path .= '/' . $uri_arr[$i];
+                }else{
+                    $class_name = $uri_arr[$i];
+                    $file_path .= '/' . $class_name . '.php';
+                    if ($i < $len){
+                        $rUri = implode('/', array_slice($uri_arr, $i+1));
+                    }
+                    break;
+                }
+            }
+            //遍历到最后的
+            if (empty($class_name)){
+                $class_name = 'index';
+                $file_path .= '/' . $class_name . '.php';
+            }
+        }
+        
+        return array('path'=>$file_path, 'class'=>$class_name, 'rUri'=>isset($rUri) ? $rUri : '');
+    }
+    
+    function get_request_uri(){
+        $start_url = ( isset($_SERVER['REQUEST_URI'])
+                    ? $_SERVER['REQUEST_URI']
+                    : $_SERVER['SCRIPT_NAME'] .
+                        ( isset($_SERVER['PATH_INFO'])
+                        ? $_SERVER['PATH_INFO']
+                        : '') .
+                            ( (isset($_SERVER['QUERY_STRING']) && ($_SERVER['QUERY_STRING'] != ''))
+                            ? '?' . $_SERVER['QUERY_STRING']
+                            : ''));
+        return $start_url;
+    }
 
-	function get_url(){
-		$protocol = 'http';
-		// If we're running on a port other than 80, i
-		// add the port number to the value returned
-		// from host_url
-		$port = 80; // Default in case not set.
-		if ( isset( $_SERVER['SERVER_PORT'] ) ) {
-			$port = $_SERVER['SERVER_PORT'];
-		}
-		$portpart = '';
-		$host = Site::get_url('hostname');
-		// if the port isn't a standard port, and isn't part of $host already, add it
-		if ( ( $port != 80 ) && ( $port != 443 ) && ( MultiByte::substr($host, MultiByte::strlen($host) - strlen($port) ) != $port ) ) {
-			$portpart = ':' . $port;
-		}
-		if ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] != 'off' ) {
-			$protocol = 'https';
-		}
-		$url = $protocol . '://' . $host . $portpart;
+    function get_url(){
+        $protocol = 'http';
+        // If we're running on a port other than 80, i
+        // add the port number to the value returned
+        // from host_url
+        $port = 80; // Default in case not set.
+        if ( isset( $_SERVER['SERVER_PORT'] ) ) {
+            $port = $_SERVER['SERVER_PORT'];
+        }
+        $portpart = '';
+        $host = Site::get_url('hostname');
+        // if the port isn't a standard port, and isn't part of $host already, add it
+        if ( ( $port != 80 ) && ( $port != 443 ) && ( MultiByte::substr($host, MultiByte::strlen($host) - strlen($port) ) != $port ) ) {
+            $portpart = ':' . $port;
+        }
+        if ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] != 'off' ) {
+            $protocol = 'https';
+        }
+        $url = $protocol . '://' . $host . $portpart;
 
-		return $url;
-	}
-	
-	//获取域名
-	function get_domain(){
-		$list = array('HTTP_HOST', 'SERVER_NAME');
-		$domain = "";
-		foreach ($list as $k){
-			if (isset($_SERVER[$k])){
-				$domain = $_SERVER[$k];
-				break;
-			}
-		}
-		
-		return $domain;
-	}
+        return $url;
+    }
+    
+    //获取域名
+    function get_domain(){
+        $list = array('HTTP_HOST', 'SERVER_NAME');
+        $domain = "";
+        foreach ($list as $k){
+            if (isset($_SERVER[$k])){
+                $domain = $_SERVER[$k];
+                break;
+            }
+        }
+        
+        return $domain;
+    }
 
-	
+    
 
     //获取rf
     static function getRf()
@@ -179,7 +179,7 @@ class Uri extends object {
     
     //获取请求方法
     static function get_method(){
-    	return $_SERVER['REQUEST_METHOD'];
+        return $_SERVER['REQUEST_METHOD'];
     }
 
     static function get_uri_string()
@@ -197,6 +197,34 @@ class Uri extends object {
             $uri_string = trim($_SERVER['PATH_INFO'], '/');
         }
         return $uri_string;
+    }
+
+    /*
+     * 给指定url添加一个参数，如果参数已经存在，则替换
+     */
+    function add_param($url, $key, $val){
+        $s_pos = strpos($url, '?');
+        if ($s_pos === false){
+            //原本没有参数
+            $url .= '?' . $key . '=' . urlencode($val);
+        }else{
+            $k_pos = strpos($url, $key, $s_pos);
+            if ($k_pos === false){
+                //参数不存在
+                $url .= '&' . $key . '=' . urlencode($val);
+            }else{
+                $e_pos = strpos($url, '&', $k_pos);
+                if ($e_pos === false){
+                    //参数在最后
+                    $url = substr($url, 0, $k_pos) . $key . '=' . urlencode($val);
+                }else{
+                    //参数在中间
+                    $url = substr($url, 0, $k_pos) . $key . '=' . urlencode($val) . substr($url, $e_pos);
+                }
+            }
+        }
+        
+        return $url;
     }
 }
 ?>
